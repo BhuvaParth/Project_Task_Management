6b3b8412b89325d3882dbe5ce658f4d3fd750808
Parth Bhuva <parth.d.bhuva2331@gmail.com>
2024-10-03 09:58:54 +0530

    resolve bugs

--- frontend/src/pages/IncompletedTasks.js
+++ frontend/src/pages/IncompletedTasks.js
@@ -1,17 +1,3 @@
-// import React from 'react'
-// import Cards from '../components/home/Cards'
-
-// const IncompletedTasks = () => {
-//   return (
-//     <>
-//       <div>
-//         <Cards home={'false'} />
-//       </div>
-//     </>
-//   )
-// }
-
-// export default IncompletedTasks
 import React from "react";
 import Cards from "../components/home/Cards";
 
